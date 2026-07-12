4bf778877bf4af0d93b360cc0589e8b1465f68af
Walmir Junior <junioroliveira8100@gmail.com>
2026-07-12 18:56:08 -0300

    implementa visualização de pacientes e validação de cartão do SUS

--- ControleDeMedicamentos.ConsoleApp/ModuloPacientes/TelaPaciente.cs
+++ ControleDeMedicamentos.ConsoleApp/ModuloPacientes/TelaPaciente.cs
@@ -11,9 +11,35 @@ public class TelaPaciente : TelaBase<Paciente>, ITelaOpcoes, ITelaCrud
 
     public override void VisualizarTodos(bool deveExibirCabecalho)
     {
-        throw new NotImplementedException();
-    }
+        if (deveExibirCabecalho)
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Visualização de Pacientes");
+            Console.WriteLine("---------------------------------");
+        }
 
+        Console.WriteLine(
+            "{0, -7} | {1, -30} | {2, -15} | {3, -18} | {4, -13}",
+            "Id", "Nome", "Telefone", "Cartao do SUS", "CPF"
+        );
+
+        List<Paciente> pacientes = repositorio.SelecionarTodos();
+
+        foreach (Paciente p in pacientes)
+        {
+            Console.WriteLine(
+                "{0, -7} | {1, -30} | {2, -15} | {3, -18} | {4, -13}",
+                p.Id, p.Nome, p.Telefone, p.CartaoSus, p.Cpf
+            );
+        }
+        if (deveExibirCabecalho)
+        {
+            Console.WriteLine("---------------------------------");
+            Console.Write("Digite ENTER para continuar...");
+            Console.ReadLine();
+        }
+    }
     protected override Paciente ObterDadosCadastrais()
     {
         Console.Write("Digite o nome do paciente: ");
@@ -29,5 +55,23 @@ public class TelaPaciente : TelaBase<Paciente>, ITelaOpcoes, ITelaCrud
         string cpf = Console.ReadLine() ?? string.Empty;
 
         return new Paciente(nome, telefone, cartaoSus, cpf);
+    }
+    protected override bool ExisteRegistroComInformacoesExclusivas(Paciente entidade, int? idIgnorado = null)
+    {
+        List<Paciente> registros = repositorio.SelecionarTodos();
+
+        foreach (Paciente p in registros)
+        {
+            if (p.Id != idIgnorado && p.CartaoSus == entidade.CartaoSus)
+            {
+                Console.WriteLine("---------------------------------");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Já existe um paciente cadastrado com o cartao do SUS informado!");
+                Console.ResetColor();
+                Console.WriteLine("---------------------------------");
+                return true;
+            }
+        }
+        return false;
     }
 }
