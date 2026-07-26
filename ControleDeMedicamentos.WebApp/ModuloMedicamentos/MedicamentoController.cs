45d3c282131a6fb7d0e37f25144f1ca14c4981d7
Walmir Junior <junioroliveira8100@gmail.com>
2026-07-26 20:10:23 -0300

    adiciona lista de mnedicamentos com acoes de Excluir e Editar na web

--- ControleDeMedicamentos.WebApp/ModuloMedicamentos/MedicamentoController.cs
+++ ControleDeMedicamentos.WebApp/ModuloMedicamentos/MedicamentoController.cs
@@ -1,12 +1,24 @@
+using ControleDeMedicamentos.WebApp.Compartilhado.Arquivos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeMedicamentos.WebApp.ModuloMedicamentos;
 
 public sealed class MedicamentoController : Controller
 {
+    private readonly RepositorioMedicamentoEmArquivo repositorio;
+    public MedicamentoController()
+    {
+        ContextoJson contexto = new ContextoJson();
+
+        contexto.Carregar();
+
+        repositorio = new RepositorioMedicamentoEmArquivo(contexto);
+    }
     [HttpGet]
     public ActionResult Listar()
     {
-        return View();
+        List<Medicamento> medicamentos = repositorio.SelecionarTodos();
+
+        return View(medicamentos);
     }
 }
