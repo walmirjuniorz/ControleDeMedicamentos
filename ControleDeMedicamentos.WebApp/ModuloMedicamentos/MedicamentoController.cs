36b13015fcc77de5c9701a5f737cb240ea45df98
Walmir Junior <junioroliveira8100@gmail.com>
2026-07-26 20:41:44 -0300

    adiciona cadastro de medicamento

--- ControleDeMedicamentos.WebApp/ModuloMedicamentos/MedicamentoController.cs
+++ ControleDeMedicamentos.WebApp/ModuloMedicamentos/MedicamentoController.cs
@@ -1,24 +1,47 @@
 using ControleDeMedicamentos.WebApp.Compartilhado.Arquivos;
+using ControleDeMedicamentos.WebApp.ModuloFornecedores;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ControleDeMedicamentos.WebApp.ModuloMedicamentos;
 
 public sealed class MedicamentoController : Controller
 {
-    private readonly RepositorioMedicamentoEmArquivo repositorio;
+    private readonly RepositorioMedicamentoEmArquivo repositorioMedicamento;
+    private readonly RepositorioFornecedorEmArquivo repositorioFornecedor;
     public MedicamentoController()
     {
         ContextoJson contexto = new ContextoJson();
 
         contexto.Carregar();
 
-        repositorio = new RepositorioMedicamentoEmArquivo(contexto);
+        repositorioMedicamento = new RepositorioMedicamentoEmArquivo(contexto);
+        repositorioFornecedor = new RepositorioFornecedorEmArquivo(contexto);
+
     }
     [HttpGet]
     public ActionResult Listar()
     {
-        List<Medicamento> medicamentos = repositorio.SelecionarTodos();
+        List<Medicamento> medicamentos = repositorioMedicamento.SelecionarTodos();
 
         return View(medicamentos);
+    }
+    [HttpGet]
+    public ActionResult Cadastrar()
+    {
+        return View();
+    }
+    [HttpPost]
+    public ActionResult Cadastrar(string nome, string descricao, int fornecedorId)
+    {
+        Fornecedor? fornecedor = repositorioFornecedor.SelecionarPorId(fornecedorId);
+
+        if (fornecedor == null)
+            return NotFound();
+
+        Medicamento medicamento = new Medicamento(nome, descricao, fornecedor);
+
+        repositorioMedicamento.Cadastrar(medicamento);
+
+        return RedirectToAction(nameof(Listar));
     }
 }
